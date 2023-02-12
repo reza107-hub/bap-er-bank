90af9f6161e8156ba43df30ad37990920782d3b0
reza107-hub <rashidaminur104@gmail.com>
2023-02-12 20:58:29 +0600

    error handle

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,11 @@
 document.getElementById('btn-deposit').addEventListener('click', function(){
-    const inputAmount = parseFloat(document.getElementById('deposit').value);
     
+    const inputAmount = parseFloat(document.getElementById('deposit').value);
+    document.getElementById('deposit').value = '';
+    if (isNaN(inputAmount)){
+        alert('Input a valid number')
+        return;
+    }
 
     const depositAmount = parseFloat(document.getElementById('deposit-amount').innerText);
     
@@ -8,6 +13,4 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
     document.getElementById('deposit-amount').innerText = totalDepositAmount;
 
     document.getElementById('Balance').innerText = parseFloat(document.getElementById('Balance').innerText) + inputAmount;
-
-    document.getElementById('deposit').value = '';
 })
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,12 +1,24 @@
 document.getElementById('btn-withdraw').addEventListener('click', function(){
     const inputAmount = parseFloat(document.getElementById('withdraw').value);
+    document.getElementById('withdraw').value = '';
+    if ((isNaN(inputAmount))){
+        alert('Input a valid number')
+        return;
+    }
 
     const withdrawAmount = parseFloat(document.getElementById('withdraw-amount').innerText);
-    
+    const totalBAlance = parseFloat(document.getElementById('Balance').innerText);
+
+    if(inputAmount > totalBAlance){
+        alert('You does not have that balance')
+        return;
+    }
+
+
     const totalWithdrawAmount = inputAmount + withdrawAmount;
     document.getElementById('withdraw-amount').innerText = totalWithdrawAmount;
 
     document.getElementById('Balance').innerText = parseFloat(document.getElementById('Balance').innerText) - inputAmount;
 
-    document.getElementById('withdraw').value = '';
+    
 })
